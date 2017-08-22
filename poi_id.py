e2766bca53e9dabc0418e1e94e602107603e213b
Miss-ada <wenjia.ma1991@gmail.com>
2017-08-22 16:29:09 -0700

    Add files via upload

--- poi_id.py
+++ poi_id.py
@@ -4,6 +4,7 @@ import sys
 import pickle
 sys.path.append("../final_project")
 from tester import dump_classifier_and_data
+from tester import test_classifier
 
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
@@ -29,23 +30,39 @@ for employee in data_dict:
 
 print count
 
-### Task 2: Remove outliers
-
+##check the allocation between the POI and non-POI
 import pandas
 import seaborn as sns
 
 data_fm=pandas.DataFrame.from_dict(data_dict, orient='columns', dtype=None)
 data_fm=data_fm.T
-sns.boxplot(data_fm)
+
+poi_data_frame=data_fm.loc[data_fm['poi'] ==True]
+nonpoi_data_frame=data_fm.loc[data_fm['poi'] ==False]
+
+usefulpoi=poi_data_frame.drop(poi_data_frame.columns[[3, 4, 6,7,8,9,13,15,17,18]], axis=1)
+usefulnonpoi=nonpoi_data_frame.drop(nonpoi_data_frame.columns[[3, 4, 6,7,8,9,13,15,17,18]], axis=1)
+poi_boxplot=sns.boxplot(usefulpoi)
+poi_boxplot.set(ylim=(0, 10**8))
+
+poi_number=len(poi_data_frame)
+
+nonpoi_boxplot=sns.boxplot(usefulnonpoi)
+nonpoi_boxplot.set(ylim=(0, 10**8))
+
+
+
+### Task 2: Remove outliers
 
 
 for employee in data_dict:
     print employee
 
 data_dict["TOTAL"]
-
+data_dict["THE TRAVEL AGENCY IN THE PARK"]
 data_dict.pop("TOTAL", None)
-
+data_dict.pop("THE TRAVEL AGENCY IN THE PARK", None)
+data_dict.pop("LOCKHART EUGENE E", None)
 
 ### Task 3: Create new feature(s)
 
@@ -69,75 +86,11 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test=cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
+# from sklearn import cross_validation
+# features_train, features_test, labels_train, labels_test=cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
 ##better split
 # from sklearn.cross_validation import StratifiedShuffleSplit
 # StratifiedShuffleSplit(y,n_iter=10, test_size=0.5, random_state=0)
-
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
-
-
-##use decision tree
-
-from sklearn import tree
-from sklearn import preprocessing
-
-scaler = preprocessing.MinMaxScaler()
-scaler.fit_transform(features)
-
-updated_features_list=[]
-clf=tree.DecisionTreeClassifier()
-clf.fit(features_train,labels_train)
-importance=clf.feature_importances_
-for i, feature_importance in enumerate(clf.feature_importances_):
-    print features_list[i+1]
-    print i, feature_importance
-
-
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.feature_selection import SelectKBest
-from sklearn.decomposition import PCA
-from sklearn.pipeline import Pipeline
-from sklearn import svm
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
-
-
-
-pca = PCA()
-skb = SelectKBest()
-svr = svm.SVC()
-nb = GaussianNB()
-
-
-#features_selected=[features_list[i+1] for i in skb.get_support(indices=True)]
-
-## use pipeline
-
-pipeline = Pipeline(steps=[('scaling',scaler),("SKB", skb),("PCA", pca), ("NB", nb)])
-
-
-parameters = {"PCA__n_components": [2, 4, 6, 8],
-              "PCA__whiten": [True],
-              "SKB__k": [8, 10, 14, 16, 18]}
-              #'SVR__kernel':('linear', 'rbf'), 'SVR__C':[1, 10, 100, 1000]}
-
-
-
-### Task 5: Tune your classifier to achieve better than .3 precision and recall
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-
-# Example starting point. Try investigating other evaluation techniques!
-
 from sklearn.cross_validation import train_test_split,StratifiedShuffleSplit
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
@@ -151,6 +104,79 @@ sss = StratifiedShuffleSplit(
     test_size = 0.5,
     random_state = 0
     )
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
+
+
+##use decision tree
+
+from sklearn import tree
+
+updated_features_list=['poi']
+clf=tree.DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+importance=clf.feature_importances_
+for i, feature_importance in enumerate(clf.feature_importances_):
+    print features_list[i+1], feature_importance
+    if feature_importance !=0.0:
+        updated_features_list.append(features_list[i])
+
+# ### Extract features and labels from dataset with updated_features_list.
+# data = featureFormat(my_dataset, updated_features_list, sort_keys=True)
+# labels, features = targetFeatureSplit(data)
+# features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels,
+#                                                                                                  test_size=0.3,
+#                                                                                                  random_state=42)
+
+
+
+# Provided to give you a starting point. Try a variety of classifiers.
+from sklearn.feature_selection import SelectKBest
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn import svm
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+
+from sklearn import preprocessing
+
+scaler = preprocessing.MinMaxScaler()
+scaler.fit_transform(features)
+
+pca = PCA()
+skb = SelectKBest()
+svr = svm.SVC()
+nb = GaussianNB()
+
+
+#features_selected=[features_list[i+1] for i in skb.get_support(indices=True)]
+
+## use pipeline to pass the result of scaling to selector and pass the selected features to PCA and then to GaussianNB()
+
+pipeline = Pipeline(steps=[('scaling',scaler),("SKB", skb),("PCA", pca), ("NB", nb)])
+
+
+parameters = {"PCA__n_components": [2, 4, 6, 8],
+              "PCA__whiten": [True],
+              "SKB__k": [12]}
+
+# SKB_k turned within  8, 10, 12, 14, 16, 18 and 12 is the best. the automatic selected is 10.
+
+# 'SVR__kernel':('linear', 'rbf'), 'SVR__C':[1, 10, 100, 1000]}
+
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
+### using our testing script. Check the tester.py script in the final project
+### folder for details on the evaluation method, especially the test_classifier
+### function. Because of the small size of the dataset, the script uses
+### stratified shuffle split cross validation. For more info:
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+# Example starting point. Try investigating other evaluation techniques!
+
 gs=GridSearchCV(pipeline,
                 param_grid=parameters,
                 scoring="f1",
@@ -161,24 +187,29 @@ labels_predictions=gs.predict(features_test)
 
 clf=gs.best_estimator_
 
+##find the parameter in SKB
+skb_step = gs.best_estimator_.named_steps['SKB']
+features_selected=[features_list[i+1] for i in skb_step.get_support(indices=True)]
+
 clf.fit(features_train,labels_train)
 pred = clf.predict(features_test)
 clf_score=gs.score(features_test, labels_test)
 
+
 test_classifier(clf, my_dataset, features_list, folds = 1000)
 
-import cPickle
-f = file('my_dataset.pkl', 'wb')
-cPickle.dump(my_dataset, f, protocol=cPickle.HIGHEST_PROTOCOL)
-f.close()
-
-fd = file('my_feature_list.pkl', 'wb')
-cPickle.dump(features_list, fd, protocol=cPickle.HIGHEST_PROTOCOL)
-fd.close()
-
-fc=file('my_classifier.pkl','wb')
-cPickle.dump(clf, fc, protocol=cPickle.HIGHEST_PROTOCOL)
-fc.close()
+# import cPickle
+# f = file('my_dataset.pkl', 'wb')
+# cPickle.dump(my_dataset, f, protocol=cPickle.HIGHEST_PROTOCOL)
+# f.close()
+#
+# fd = file('my_feature_list.pkl', 'wb')
+# cPickle.dump(features_list, fd, protocol=cPickle.HIGHEST_PROTOCOL)
+# fd.close()
+#
+# fc=file('my_classifier.pkl','wb')
+# cPickle.dump(clf, fc, protocol=cPickle.HIGHEST_PROTOCOL)
+# fc.close()
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
